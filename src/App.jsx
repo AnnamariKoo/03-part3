863c2da7e9660fc22fa78de9fcc4a834028db05b
AnnamariKoo <163762866+AnnamariKoo@users.noreply.github.com>
2024-09-04 12:51:07 +0300

    Tehtävä tehty tähän asti saatujen ohjeiden mukaan.

--- src/App.jsx
+++ src/App.jsx
@@ -7,32 +7,31 @@ const History = (props) => {
   return <div>button press history: {props.allClicks.join(" ")}</div>;
 };
 
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
 const App = () => {
   const [left, setLeft] = useState(0);
   const [right, setRight] = useState(0);
   const [allClicks, setAll] = useState([]);
-  const [total, setTotal] = useState(0);
 
   const handleLeftClick = () => {
     setAll(allClicks.concat("L"));
-    const updatedLeft = left + 1;
-    setLeft(updatedLeft);
-    setTotal(updatedLeft + right);
+    setLeft(left + 1);
   };
 
   const handleRightClick = () => {
     setAll(allClicks.concat("R"));
-    const updatedRight = right + 1;
-    setRight(updatedRight);
-    setTotal(left + updatedRight);
+    setRight(right + 1);
   };
 
   return (
     <div>
       <div>
         {left}
-        <button onClick={handleLeftClick}>left</button>
-        <button onClick={handleRightClick}>right</button>
+        <Button handleClick={handleLeftClick} text="left" />
+        <Button handleClick={handleRightClick} text="right" />
         {right}
         <History allClicks={allClicks} />
       </div>
